edb51aa4a042fd176046aeb92a210c8a6cc94836
unknown <jamesothniel7@gmail.com>
2023-09-24 17:09:17 -0700

    second commit

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
-  return <div>Demo</div>;
+  const [article, setArticle] = useState({ url: "", summary: "" });
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+
+      setArticle(newArticle);
+
+      console.log(newArticle);
+    }
+  };
+
+  return (
+    <section className="mt-8 w-full max-w-xl">
+      {/* Search */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-content items-center"
+          onSubmit={handleSubmit}>
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+
+          <input
+            type="url"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">{`>>>`}</button>
+        </form>
+        {/* Browse URL History */}
+      </div>
+      {/* Display Results */}
+    </section>
+  );
 };
 
 export default Demo;
